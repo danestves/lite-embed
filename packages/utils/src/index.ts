bebcd5afb5d1c73f2980e00a66c1211bc27a3ecc
Daniel Esteves <me+github@danestves.com>
2022-03-07 01:52:56 -0400

    feat: adding youtube vars function

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,8 @@
 export { default as addPrefetch } from "./add-prefetch";
+export * from "./add-prefetch";
+
 export { default as getYouTubeId } from "./get-youtube-id";
+export * from "./get-youtube-id";
+
+export { default as getYoutubeVars } from "./get-youtube-vars";
+export * from "./get-youtube-vars";
