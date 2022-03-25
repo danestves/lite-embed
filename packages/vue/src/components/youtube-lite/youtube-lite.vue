4ccdd970e860f1123bb54cafb63fe9d63255cb16
Daniel Esteves <me+github@danestves.com>
2022-03-25 01:12:16 -0400

    fix: moving youtube gradient css to component itself

--- packages/vue/src/components/youtube-lite/youtube-lite.vue
+++ packages/vue/src/components/youtube-lite/youtube-lite.vue
@@ -126,6 +126,23 @@ const addIframe = () => {
 </template>
 
 <style scoped>
+[data-le="aspect-ratio-item"]::before {
+  content: "";
+  background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAADGCAYAAAAT+OqFAAAAdklEQVQoz42QQQ7AIAgEF/T/D+kbq/RWAlnQyyazA4aoAB4FsBSA/bFjuF1EOL7VbrIrBuusmrt4ZZORfb6ehbWdnRHEIiITaEUKa5EJqUakRSaEYBJSCY2dEstQY7AuxahwXFrvZmWl2rh4JZ07z9dLtesfNj5q0FU3A5ObbwAAAABJRU5ErkJggg==");
+  background-repeat: repeat-x;
+  background-position: top;
+  box-sizing: content-box;
+  display: block;
+  height: 60px;
+  padding-bottom: 50px;
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  transition: all 0.25s cubic-bezier(0, 0, 0.2, 1);
+  width: 100%;
+  z-index: 25;
+}
+
 [data-le="iframe"] {
   border: 0;
   height: 100%;
