29824ba59b7b8b8607b52b8d8fb876ff31c81b99
Daniel Esteves <me+github@danestves.com>
2022-03-25 02:56:12 -0400

    fix: removing optional chaining to avoid errors

--- packages/utils/src/get-vimeo-poster-url.ts
+++ packages/utils/src/get-vimeo-poster-url.ts
@@ -20,7 +20,7 @@ async function getVimeoPosterUrl({
     `https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/${videoId}`
   ).then((res) => res.json());
 
-  let thumbnailUrl = result?.thumbnail_url || "";
+  let thumbnailUrl = result.thumbnail_url || "";
 
   // Replace the current size with the poster variable
   // i.e https://i.vimeocdn.com/video/554912674-5b4fc6c5c9041034676a60ecf1cc987c8a79e35ddb4352782efaa7f1cf96f107-d_295x166
